bfb0ce3aa3b0f797fddd09e6914198858d3d1c67
ravlick <svtrojan16@gmail.com>
2020-11-04 20:54:42 +0200

    авторизация пользователя конец

--- js/main.js
+++ js/main.js
@@ -11,12 +11,53 @@ function toggleModal() {
 const buttonAuth = document.querySelector('.button-auth');
 const modalAuth = document.querySelector('.modal-auth');
 const closeAuth = document.querySelector('.close-auth');
+const logInForm = document.querySelector('#logInForm');
+const loginInput = document.querySelector('#login');
+const userName = document.querySelector('.user-name');
+const buttonOut = document.querySelector('.button-out');
+let login = '';
 
-
-function  toggleModalAuth(){
+function toggleModalAuth(){
   modalAuth.classList.toggle('is-open');
 }
-buttonAuth.addEventListener('click', toggleModalAuth);
-closeAuth.addEventListener('click', toggleModalAuth);
 
-console.dir('buttonAuth');
+
+function authorized(){
+  console.log(login);
+  userName.textContent = login;
+  buttonAuth.style.display='none';
+  buttonOut.style.display = 'block';
+  userName.style.display = 'inline';
+
+  buttonOut.addEventListener('click' , logOut);
+  function logOut(){
+   login = '';
+
+    buttonAuth.style.display='';
+    buttonOut.style.display = '';
+    userName.style.display = '';
+    checkAuth();
+  }
+}
+
+function notAuthorized(){
+  console.log('not');
+  function logIn(event){
+    event.preventDefault();
+    login = loginInput.value;
+    toggleModalAuth();
+    checkAuth();
+  }
+  buttonAuth.addEventListener('click', toggleModalAuth);
+  closeAuth.addEventListener('click', toggleModalAuth);
+  logInForm.addEventListener('submit',logIn);
+}
+function checkAuth(){
+  if (login){
+    authorized();
+  }else{
+    notAuthorized();
+  }
+}
+
+checkAuth();
